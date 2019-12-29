d3615a93e8c0e2bab0334825b5e0647676f993b2
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-29 01:49:28 -0300

    Drawer simple for navigation Todo and Context

---
+++ lib/src/drawer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_example/src/context/context.list.stateless.dart';
+import 'package:flutter_example/src/todo/todo.list.stateless.dart';
+
+ListView drawer(BuildContext context) {
+  return ListView(children: <Widget>[
+    DrawerHeader(
+      child: Text('GTD'),
+    ),
+    ListTile(
+      title: Text('Todo'),
+      onTap: () {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => TodoListPage()));
+      },
+    ),
+    ListTile(
+      title: Text('Context'),
+      onTap: () {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => ContextListPage()));
+      },
+    )
+  ]);
+}
